c798e2ae05b81be966da8cf8bd4b82e63d960325
jaiv shah <jaiv.maurya@gmail.com>
2021-08-11 13:36:58 -0400

    no message

--- src/main/java/com/example/myspringapp/repository/UserRepository.java
+++ src/main/java/com/example/myspringapp/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
-    
+
     public User getUser(){
         return new User("Jaiv", "16 Gunton Dr", 21, "jaiv.jpg");
     }
